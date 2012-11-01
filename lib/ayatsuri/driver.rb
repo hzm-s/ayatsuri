92360e7b442b5b79a2cea58d8434727617729f21
h4zime <h4zime@gmail.com>
2012-11-01 23:04:36 +0900

    add query methods module for driver

--- lib/ayatsuri/driver.rb
+++ lib/ayatsuri/driver.rb
@@ -1,13 +1,54 @@
 require 'win32ole'
-require 'singleton'
 
 module Ayatsuri
 	class Driver
-		include Singleton
+
+		module QueryMethods
+
+			class << self
+
+				def encoding=(encoding)
+					Encoding.default_internal = encoding
+				end
+			end
+
+			def get_active_window_handle
+				ole.WinGetHandle("[active]")
+			end
+
+			def get_active_window_title
+				ole.WinGetTitle("[active]").encode
+			end
+
+			def get_active_window_text
+				ole.WinGetText("[active]").encode
+			end
+
+			def get_control_text(control_id)
+				ole.ControlGetText("[active]", "", control_id).encode
+			end
+		end
+	end
+
+	class Driver
+		@instance = nil
+
+		class << self
+
+			def instance
+				@instance ||= new
+			end
+
+			def flush
+				@instance = nil
+			end
+		end
 
 		def initialize
 			@ole = WIN32OLE.new('AutoItX3.Control')
 		end
+
+		private_class_method :new
 
 		def invoke(method, args)
 			ole.send(method, *args)
--- spec/ayatsuri/driver_spec.rb
+++ spec/ayatsuri/driver_spec.rb
@@ -1,23 +1,80 @@
 require 'spec_helper'
 
 module Ayatsuri
+	class Driver
+		
+		describe QueryMethods do
+			let(:model) { Object.new.extend(described_class) }
+
+			before do
+				model.stub(:ole) { stub_ole }
+				raw_string.stub(:encode) { encoded_string }
+			end
+
+			let(:stub_ole) { mock 'ole for autoit' }
+			let(:raw_string) { mock 'raw string' }
+			let(:encoded_string) { mock 'encoded string' }
+
+			let(:control_id) { mock 'control id' }
+
+			describe ".encoding=" do
+				subject { described_class.encoding = enc }
+				let(:enc) { Encoding::UTF_8 }
+				it { subject; Encoding.default_internal.should == enc }
+			end
+
+			describe "#get_active_window_title" do
+				subject { model.get_active_window_title }
+				before do
+					stub_ole.stub(:WinGetTitle).with("[active]") { raw_string }
+				end
+				it { should == encoded_string }
+			end
+
+			describe "#get_active_window_handle" do
+				subject { model.get_active_window_handle }
+				before { stub_ole.stub(:WinGetHandle).with("[active]") { raw_string } }
+				it { should == raw_string }
+			end
+
+			describe "#get_active_window_text" do
+				subject { model.get_active_window_text }
+				before { stub_ole.stub(:WinGetText).with("[active]") { raw_string } }
+				it { should == encoded_string }
+			end
+
+			describe "#get_control_text" do
+				subject { model.get_control_text control_id }
+				before { stub_ole.stub(:ControlGetText).with("[active]", "", control_id) { raw_string } }
+				it { should == encoded_string }
+			end
+		end
+	end
+
 	describe Driver do
 		let(:model) { described_class.instance }
 
 		before do
-			described_class.any_instance.stub(:ole) { ole }
+			described_class.any_instance.stub(:ole) { stub_ole }
 		end
 
-		let(:ole) { mock 'win32ole for autoit X3' }
+		after { described_class.flush }
+
+		let(:stub_ole) { mock 'win32ole for autoit X3' }
 
 		describe ".instance" do
 			subject { described_class.instance }
 
 			before do
-				WIN32OLE.stub(:new).with('AutoItX3.Control') { ole }
+				WIN32OLE.stub(:new).with('AutoItX3.Control') { stub_ole }
 			end
 
 			it { should == model }
+		end
+
+		describe ".new" do
+			subject { described_class.new }
+			it { expect { subject }.to raise_error(NoMethodError) }
 		end
 
 		describe "#invoke" do
@@ -27,7 +84,7 @@ module Ayatsuri
 			let(:args) { [:arg1, :arg2, :arg3] }
 
 			before do
-				ole.should_receive(:send).with(method_name, *args) { result }
+				stub_ole.should_receive(:send).with(method_name, *args) { result }
 			end
 
 			context "when successful" do
